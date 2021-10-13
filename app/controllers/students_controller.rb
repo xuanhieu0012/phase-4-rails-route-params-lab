2a5eee5241b8ef7a2f14c5c4f7e22ac0f507b8ff
Hieu <xuanhieu0012@gmail.com>
2021-10-13 09:23:12 -0500

    commit tree 314fedac8a4a2a8b48662570984aa7fdc5237605

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,21 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
-    render json: students
+    if params[:name]
+      array = []
+      array << student = Student.all.find_by(first_name: params[:name].capitalize) || Student.all.find_by(last_name: params[:name].capitalize) 
+      
+      render json: array
+    else
+      students = Student.all
+      render json: students
+    end
+    
   end
+  def details
+    student = Student.find(params[:id])
+    render json: student
+  end
+
 
 end
